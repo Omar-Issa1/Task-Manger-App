f490dab7dfc9317cdc1684b26c33863502f6bccc
Omar Issa <omom2168@gmail.com>
2025-10-15 19:11:14 +0300

    deploy to vercel

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
-import express from "express";
-const app = express();
-import tasks from "./routes/tasks.js";
-import connectDB from "./db/connect.js";
-import dotenv from "dotenv";
-dotenv.config();
-import notFound from "./middleware/not-found.js";
-import errorHandler from "./middleware/error-handler.js";
+// import express from "express";
+// import tasks from "./routes/tasks.js";
+// import connectDB from "./db/connect.js";
+// import dotenv from "dotenv";
+// import notFound from "./middleware/not-found.js";
+// import errorHandler from "./middleware/error-handler.js";
+// const app = express();
+// dotenv.config();
+// //routes
+// app.use(express.json());
+// app.use("/api/v1/tasks", tasks);
+// app.use(express.static("./public"));
 
-//routes
-app.use(express.json());
-app.use("/api/v1/tasks", tasks);
-app.use(express.static("./public"));
+// // middleware
 
-// middleware
+// app.use(notFound);
+// app.use(errorHandler);
+// const port = process.env.PORT || 3000;
 
-app.use(notFound);
-app.use(errorHandler);
-const port = process.env.PORT || 3000;
+// const start = async () => {
+//   try {
+//     const connected = await connectDB("mongodb://127.0.0.1:27017/Task_Manger");
+//     if (connected) {
+//       console.log("Connection done");
+//     }
+//     app.listen(port, () => console.log(`server is running on port ${port}`));
+//   } catch (error) {
+//     console.error("Cannot connect to database or the server is down:", error);
+//   }
+// };
 
-const start = async () => {
-  try {
-    const connected = await connectDB(process.env.MONGO_URI);
-    if (connected) {
-      console.log("Connection done");
-    }
-    app.listen(port, () => console.log(`server is running on port ${port}`));
-  } catch (error) {
-    console.error("Cannot connect to database or the server is down:", error);
-  }
-};
-
-start();
+// start();
