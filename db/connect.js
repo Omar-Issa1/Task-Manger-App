c1afbd700c5c830131a1b358b9f52c90308f1dae
Omar Issa <omom2168@gmail.com>
2025-10-15 19:33:42 +0300

    deploy to vercel

--- db/connect.js
+++ db/connect.js
@@ -1,14 +1,25 @@
 import mongoose from "mongoose";
 
-const connectDB = (url) => {
-  return mongoose.connect(url);
-};
+let cached = global.mongoose;
 
-// .then(() => {
-//   console.log("connected to db");
-// })
-// .catch((err) => {
-//   console.log(err);
-// });
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const connectDB = async (uri) => {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
 
 export default connectDB;
